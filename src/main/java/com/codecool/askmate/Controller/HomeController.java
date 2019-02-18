cf18f319930a9a2e96a644f2bfacae12b5457897
Daniel Mielczarek <daniel1112m@gmail.com>
2019-02-18 14:06:56 +0100

    Added bootsrap

--- src/main/java/com/codecool/askmate/Controller/HomeController.java
+++ src/main/java/com/codecool/askmate/Controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/")
     public String showHomePage(){
         return "homePage";
     }
