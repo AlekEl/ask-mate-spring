1ddcf1578e349f96bbfc23a8a457a32dee9b5e6d
quadRed <maciejpatejmail@gmail.com>
2019-02-19 11:55:10 +0100

    Create saving questions service

--- src/main/java/com/codecool/askmate/Controller/HomeController.java
+++ src/main/java/com/codecool/askmate/Controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.codecool.askmate.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = "/add-question")
     public String addQuestion(){
         return "addQuestionPage";
+    }
+
+    @PostMapping(value = "/save-question")
+    public String saveQuestion(){
+        return "redirect:/";
     }
 
     @RequestMapping(value = "/")
