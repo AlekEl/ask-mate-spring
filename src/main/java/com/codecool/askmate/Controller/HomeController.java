27737f9178e6ee3c8f54725237b82b71aa357671
Daniel Mielczarek <daniel1112m@gmail.com>
2019-02-19 10:39:41 +0100

    Merge conflicts resolve

--- src/main/java/com/codecool/askmate/Controller/HomeController.java
+++ src/main/java/com/codecool/askmate/Controller/HomeController.java
@@ -1,12 +1,11 @@
 package com.codecool.askmate.Controller;
 
-<<<<<<< HEAD
+
 import com.codecool.askmate.Model.Question;
 import com.codecool.askmate.Services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-=======
->>>>>>> 574d4a0bb0716687fed624e2aacd7e039a8fbd49
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,23 +13,19 @@ import java.util.Collection;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/add-question")
-    public String addQuestion(){
-        return "addQuestionPage";
-    }
 
-<<<<<<< HEAD
     @Autowired
     QuestionService questionService;
+
+    @RequestMapping(value = "/add-question")
+    public String addQuestion() {
+        return "addQuestionPage";
+    }
 
     @RequestMapping("/")
     public String showHomePage(Model model) {
         Collection<Question> questions = questionService.getAllQuetions();
         model.addAttribute("questions", questions);
-=======
-    @RequestMapping(value = "/")
-    public String showHomePage(){
->>>>>>> 574d4a0bb0716687fed624e2aacd7e039a8fbd49
         return "homePage";
     }
 
